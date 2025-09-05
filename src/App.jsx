8bdc40b131e8c137e444ca9cbb03fde0ec563022
rahulm2210 <rahul486mejari@gmail.com>
2025-09-05 22:57:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+//import DndDraggableExample from "./DndDraggableExample";
+import DndDraggableExample from "https://raw.githubusercontent.com/rahulm2210/cms1/refs/heads/main/DndDraggableExample.jsx";
+
+function App() {
+  return (
+    <>
+      <DndDraggableExample />
+    </>
+  );
+}
+
+export default App;
