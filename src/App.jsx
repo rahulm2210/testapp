2f9c67d733025d5f0704bab4477abec28df247cd
rahulm2210 <37929095+rahulm2210@users.noreply.github.com>
2025-09-05 23:18:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-//import DndDraggableExample from "./DndDraggableExample";
-import DndDraggableExample from "https://raw.githubusercontent.com/rahulm2210/cms1/refs/heads/main/DndDraggableExample.jsx";
+import DndDraggableExample from "./DndDraggableExample";
+//import DndDraggableExample from "https://raw.githubusercontent.com/rahulm2210/cms1/refs/heads/main/DndDraggableExample.jsx";
 
 function App() {
   return (
